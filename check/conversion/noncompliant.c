a714eba94030782440e73d0772cff9e6788d085f
halcyoncheng <cgy429415591@outlook.com>
2018-07-10 17:00:02 -0400

    code for converison

--- check/conversion/noncompliant.c
+++ check/conversion/noncompliant.c
@@ -1,15 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * @file Test for conversion warinings.
+ * 
+ * @par Risk Assessment
+ * 
+ * | Severity | Likelihood | Remediation Cost | Priority | Level |
+ * | -------- | ---------- | ---------------- | -------- | ----- |
+ * | High     | Probable   | Low              | P6       | L2    |
+ * 
+ * @see SEI CERT C Coding Standard: Rule INT31-C. Ensure that integer
+ * conversions do not result in lost or misinterpreted data
+ * https://wiki.sei.cmu.edu/confluence/display/c/INT31-C.+Ensure+that+integer+conversions+do+not+result+in+lost+or+misinterpreted+data
+ * 
+ */
 
-int main(){
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * Test warning when implicit conversion changes value.
+ */
+static void qiip_conversion_test(void);
+
+
+/**
+ * Main entry point to program.
+ */
+signed main(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+static void qiip_conversion_test(void)
+  {
+#ifndef QIIP_FIX
     int num = -10;
     num = num + 5;
-    unsigned score = num; /* Implicit conversion that changes value. */
+    unsigned score = num;
+#else
+    int num = -10;
+    num = num + 5;
+    signed score = num;
+#endif
     if(score < 0){
         printf("Success!\n");
     }
     else{
         printf("Failure!\n");
     }
-}
+  }
+
+
+signed main()
+  {
+      qiip_conversion_test();
+  }
+
 
