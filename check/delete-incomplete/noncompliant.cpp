77ca76ad59e244eed4022f6c34f674ed93df32a6
halcyoncheng <cgy429415591@outlook.com>
2018-07-11 11:14:02 -0400

    code for delete-incomplete

--- check/delete-incomplete/noncompliant.cpp
+++ check/delete-incomplete/noncompliant.cpp
@@ -1,13 +1,69 @@
+/**
+ * @file Test for delete incomplete warinings.
+ * 
+ * @par Risk Assessment
+ * 
+ * | Severity | Likelihood | Remediation Cost | Priority | Level |
+ * | -------- | ---------- | ---------------- | -------- | ----- |
+ * | Medium   | Unlikely   | Medium           | P4       | L3    |
+ * 
+ * @see SEI CERT C Coding Standard: Rule ARR30-C. Do not cast or delete
+ * pointers to incomplete classes
+ * https://wiki.sei.cmu.edu/confluence/display/cplusplus/EXP57-CPP.+Do+not+cast+or+delete+pointers+to+incomplete+classes
+ * 
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <memory>
 
-int main()
-{
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * Test warning when deleting pointer to incomplete classes.
+ */
+static void qiip_deleteincomplete_test(void);
+
+/**
+ * Main entry point to program.
+ */
+signed main(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+static void qiip_deleteincomplete_test(void)
+  {
+#ifndef QIIP_FIX
     class Handle
-    {
+      {
         class Body *impl; // Declaration of a pointer to an incomplete class
     public:
         ~Handle() { delete impl; } // Deletion of pointer to an incomplete class
                                    // ...
-    };
-}
+      };
+#else
+    class Handle 
+      {
+        std::shared_ptr<class Body> impl;
+        public:
+            Handle();
+            ~Handle() {}
+      };
+#endif
+
+  }
+
+
+signed main()
+  {
+      qiip_deleteincomplete_test();
+  }
