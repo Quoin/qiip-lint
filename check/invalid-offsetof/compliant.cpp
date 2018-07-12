fde8a839eac8d10aa2b9c93d5a00477b137acacd
halcyoncheng <cgy429415591@outlook.com>
2018-07-12 15:34:42 -0400

    Code for invalid-offsetof

--- check/invalid-offsetof/compliant.cpp
+++ check/invalid-offsetof/compliant.cpp
@@ -1,16 +1,57 @@
-#include <cstddef>
- 
-struct D {
-  virtual void f() {}
-  struct InnerStandardLayout {
-    int i;
-  } inner;
-};
- 
-void f() {
-  size_t off = offsetof(D::InnerStandardLayout, i);
-  // ...
-}
-int main(){
+/**
+ * @file Test for invalid offset of warinings.
+ * 
+ * @par Risk Assessment
+ * 
+ * | Severity | Likelihood | Remediation Cost | Priority | Level |
+ * | -------- | ---------- | ---------------- | -------- | ----- |
+ * | Medium   | Unlikely   | Medium           | P4       | L3    |
+ * 
+ * @see SEI CERT C Coding Standard: Rule EXP59-CPP. Use offsetof() on
+ * valid types and members
+ * https://wiki.sei.cmu.edu/confluence/display/cplusplus/EXP59-CPP.+Use+offsetof%28%29+on+valid+types+and+members
+ * 
+ */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <cstddef>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/**
+ * Main entry point to program.
+ */
+signed main(void);
+
+
+#ifdef __cplusplus
 }
+#endif
+
+
+struct D
+  {
+    virtual void f() {}
+    struct InnerStandardLayout
+      {
+        int i;
+      } inner;
+  };
+ 
+void f()
+  {
+    size_t off = offsetof(D::InnerStandardLayout, i);
+    printf("The offset is %lu", off);
+  }
+
+signed main()
+  {
+    return EXIT_SUCCESS;
+  }
+
