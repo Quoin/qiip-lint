0454fe9f476b8c6261c6be67f78ea06db5815bfd
halcyoncheng <cgy429415591@outlook.com>
2018-07-12 17:11:47 -0400

    Code for shift-count-overflow

--- check/shift-count-overflow/noncompliant.c
+++ check/shift-count-overflow/noncompliant.c
@@ -1,8 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * @file Test for shift count overflow warinings.
+ * 
+ * @par Risk Assessment
+ * 
+ * | Severity | Likelihood | Remediation Cost | Priority | Level |
+ * | -------- | ---------- | ---------------- | -------- | ----- |
+ * | Low      | Unlikely   | Medium           | P2       | L3    |
+ * 
+ * @see SEI CERT C Coding Standard: Rule INT34-C. Do not shift an expression
+ * by a negative number of bits or by greater than or equal to the number of
+ * bits that exist in the operant
+ * https://wiki.sei.cmu.edu/confluence/display/c/INT34-C.+Do+not+shift+an+expression+by+a+negative+number+of+bits+or+by+greater+than+or+equal+to+the+number+of+bits+that+exist+in+the+operand
+ * 
+ */
 
-int main(){
-    signed long a = 10;
-    signed long b = 10000;
-    return a << b;
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * Test warning when shifting expressions by count larger than the number of bits exist.
+ */
+static void qiip_shiftcountoverflow_test(void);
+
+
+/**
+ * Main entry point to program.
+ */
+signed main(void);
+
+
+#ifdef __cplusplus
 }
+#endif
+
+
+static void qiip_shiftcountoverflow_test(void)
+  {
+#ifndef QIIP_FIX
+    signed long a = 10;
+    signed long num = a << 10000;
+    printf("The number is %ld", num);
+#else
+    signed long a = 10000;
+    signed long num = a << 10;
+    printf("The number is %ld", num);
+#endif
+  }
+
+
+signed main()
+  {
+      qiip_shiftcountoverflow_test();
+  }
