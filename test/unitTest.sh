2f14dab5f348e446f7f3bf736b2565975e1468b3
halcyoncheng <cgy429415591@outlook.com>
2018-07-19 16:41:23 -0400

    basic test

--- test/unitTest.sh
+++ test/unitTest.sh
@@ -5,36 +5,11 @@
     
 function testForBitShift () {
     cd ..
-    cd itc-benchmarks/01.w_Defects
-    gcc -c bit_shift.c 2> ./temp
-    occurance=$(grep "Wshift" ./temp | wc -l)
-    echo "gcc detection:"
-    assertEquals '17' $occurance
-    clang -c bit_shift.c 2> ./temp
-    occurance=$(grep "Wshift" ./temp | wc -l)
-    echo "clang detection:"
-    assertEquals '17' $occurance
-    cppcheck bit_shift.c 2> ./temp 1> /dev/null
-    occurance=$(grep "error" ./temp | wc -l)
-    echo "cppcheck detection:"
-    assertEquals '17' $occurance
-    return 0
+    rm -f check/"div-by-zero"/noncompliant.o && make check/"div-by-zero"/noncompliant.o 2>temp.txt
+    occurance=$(grep -c "\-Wdiv-by-zero" temp.txt)
+    printf "Occurance: $occurance\n"
     
 }
-function testForBufferOverrunDynamic () {
-    gcc -c buffer_overrun_dynamic.c 2> ./temp
-    occurance=$(grep "Wshift" ./temp | wc -l)
-    echo "gcc detection:"
-    assertEquals '17' $occurance
-    clang -c buffer_overrun_dynamic.c 2> ./temp
-    occurance=$(grep "Wshift" ./temp | wc -l)
-    echo "clang detection:"
-    assertEquals '17' $occurance
-    cppcheck buffer_overrun_dynamic.c 2> ./temp 1> /dev/null
-    occurance=$(grep "error" ./temp | wc -l)
-    echo "cppcheck detection:"
-    assertEquals '17' $occurance
-    return 0
-}
+
 ## Call and Run all Tests
 . "../shunit2/shunit2"
