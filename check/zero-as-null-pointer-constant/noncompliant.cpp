96f5b73cb79a938f841b9e057ad6c6395edc268d
LeJacq, Jean Pierre <jeanpierre.lejacq@quoininc.com>
2018-07-20 11:05:31 -0400

    Converted test to normalized C++ style.

--- check/zero-as-null-pointer-constant/noncompliant.cpp
+++ check/zero-as-null-pointer-constant/noncompliant.cpp
@@ -1,69 +1,71 @@
 /**
  * @file Test for zero as null pointer warinings.
- * 
+ *
  * @par Risk Assessment
- * 
+ *
  * | Severity | Likelihood | Remediation Cost | Priority | Level |
  * | -------- | ---------- | ---------------- | -------- | ----- |
  * | Low      | Medium     | High             | P2       | L3    |
- * 
+ *
  * @see SEI CERT C Coding Standard: Rule INT36-C. Converting a pointer
- * to integer or integer to pointer
- * https://wiki.sei.cmu.edu/confluence/display/c/INT36-C.+Converting+a+pointer+to+integer+or+integer+to+pointer
- * 
+ *   to integer or integer to pointer
+ *   https://wiki.sei.cmu.edu/confluence/display/c/INT36-C.+Converting+a+pointer+to+integer+or+integer+to+pointer
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
 
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-/**
- * Test warning when using '0' as null pointer.
- */
-static void qiip_zeroasnullpointer_test(void);
+#include <exception>
+#include <cstdio>
+#include <cstdlib>
 
 
 /**
  * Main entry point to program.
  */
-signed main(void);
+auto main [[gnu::nothrow]] (void) -> signed;
 
 
-#ifdef __cplusplus
-}
-#endif
-
-
-static void qiip_zeroasnullpointer_test(void)
+namespace qiip
   {
-#ifndef QIIP_FIX
-    char* string;
-    string = (char*) 0;
-    if(string == NULL)
-      {
-        printf("Success!\n");
-      }
+    /**
+     * Test warning when using '0' as null pointer.
+     */
+    static auto zeroasnullpointer_test [[gnu::nothrow]] (void) -> void;
+  }
+
+
+static auto qiip::zeroasnullpointer_test(void) -> void
+  {
+#if !((defined QIIP_FIX) && (1 == QIIP_FIX))
+    char const * const __restrict__ string{0};
 #else
-    char* string;
-    string = (char*) NULL;
-    if(string == NULL)
+    char const * const __restrict__ string{nullptr};
+#endif
+    std::printf("string=\"%p\".\n", string);
+  }
+
+
+auto main(void) -> signed
+  {
+    auto result{EXIT_SUCCESS};
+
+    try
       {
-        printf("Success!\n");
+        qiip::zeroasnullpointer_test();
+      }
+    catch (std::exception const & __restrict__ e)
+      {
+        auto const msg{e.what()};
+        std::fprintf
+          (
+            stderr, "Failure executing tests. exception=\"%s\".\n", msg
+          );
+        result = EXIT_FAILURE;
+      }
+    catch (...)
+      {
+        std::fprintf(stderr, "Failure executing tests. Unknown exception.\n");
+        result = EXIT_FAILURE;
       }
 
-#endif
+    return result;
   }
-
-
-signed main()
-  {
-      qiip_zeroasnullpointer_test();
-  }
-
-
