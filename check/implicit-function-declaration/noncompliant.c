b0ecab2150b8f20627cb74e4093ad3b34a50c5b6
LeJacq, Jean Pierre <jeanpierre.lejacq@quoininc.com>
2018-07-21 11:08:48 -0400

    Fix that allows compilation of C as C++ when normally fatal.

--- check/implicit-function-declaration/noncompliant.c
+++ check/implicit-function-declaration/noncompliant.c
@@ -1,5 +1,5 @@
 /**
- * @file Test for implicit function declaration warinings.
+ * @file Test for implicit function declaration warnings.
  * 
  * @par Risk Assessment
  * 
@@ -8,33 +8,53 @@
  * | Low      | Unlikely   | Low              | P3       | L3    |
  * 
  * @see SEI CERT C Coding Standard: Rule DCL31-C. Declare identifiers before
- * using them
- * https://wiki.sei.cmu.edu/confluence/display/c/ARR30-C.+Do+not+form+or+use+out-of-bounds+pointers+or+array+subscripts
- * 
+ *   using them
+ *   https://wiki.sei.cmu.edu/confluence/display/c/ARR30-C.+Do+not+form+or+use+out-of-bounds+pointers+or+array+subscripts
  */
 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#if ((defined __cplusplus) || ((defined QIIP_FIX) && (1 == QIIP_FIX)))
+  #include <string.h>
+#endif
+
+
+/**
+ * Main entry point to program.
+ */
+signed main(void) __attribute__((nothrow));
 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-
 /**
- * Main entry point to program.
+ * Test warning when calling function without prototype in scope.
  */
-signed main(void);
-
+static void qiip_implicitfunctiondeclaration_test(void) __attribute__((nothrow));
 
 #ifdef __cplusplus
 }
 #endif
 
 
-
-signed main()
+signed main(void)
   {
-    char* string = (char*) malloc(sizeof(char));
-    free(string);
+    signed const result = EXIT_SUCCESS;
+
+    qiip_implicitfunctiondeclaration_test();
+
+    return result;
+  }
+
+
+static void qiip_implicitfunctiondeclaration_test(void)
+  {
+    char const msg[] = "hello, world";
+    size_t const len = strlen(msg);
+    printf("len=\"%zu\".\n", len);
   }
